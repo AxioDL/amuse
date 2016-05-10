11739d86e28bb556852f61c04fbe8cc04242d0b8
Jack Andersen <jackoalan@gmail.com>
2016-05-09 16:31:05 -1000

    AudioGroupData to use Factor5's convention, not Retro's

--- driver/main.cpp
+++ driver/main.cpp
@@ -1,17 +1,20 @@
 #include "amuse/amuse.hpp"
 #include "amuse/BooBackend.hpp"
 #include "boo/audiodev/IAudioVoiceEngine.hpp"
+#include "logvisor/logvisor.hpp"
 #include <thread>
 
 int main(int argc, char* argv[])
 {
+    logvisor::RegisterConsoleLogger();
+
     std::unique_ptr<boo::IAudioVoiceEngine> voxEngine = boo::NewAudioVoiceEngine();
     amuse::BooBackendVoiceAllocator booBackend(*voxEngine);
     amuse::Engine engine(booBackend);
 
     amuse::Voice* vox = engine.fxStart(1, 1.f, 0.f);
 
-    for (int f=0 ; f<500 ; ++f)
+    for (int f=0 ; f<300 ; ++f)
     {
         engine.pumpEngine();
         std::this_thread::sleep_for(std::chrono::milliseconds(15));
--- include/amuse/AudioGroupData.hpp
+++ include/amuse/AudioGroupData.hpp
@@ -10,17 +10,17 @@ namespace amuse
 class AudioGroupData
 {
 protected:
-    unsigned char* m_pool;
     unsigned char* m_proj;
+    unsigned char* m_pool;
     unsigned char* m_sdir;
     unsigned char* m_samp;
 public:
-    AudioGroupData(unsigned char* pool, unsigned char* proj,
+    AudioGroupData(unsigned char* proj, unsigned char* pool,
                    unsigned char* sdir, unsigned char* samp)
-    : m_pool(pool), m_proj(proj), m_sdir(sdir), m_samp(samp) {}
+    : m_proj(proj), m_pool(pool), m_sdir(sdir), m_samp(samp) {}
 
-    const unsigned char* getPool() const {return m_pool;}
     const unsigned char* getProj() const {return m_proj;}
+    const unsigned char* getPool() const {return m_pool;}
     const unsigned char* getSdir() const {return m_sdir;}
     const unsigned char* getSamp() const {return m_samp;}
 };
