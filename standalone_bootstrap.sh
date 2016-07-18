260ec5bb934a2e7c99b24c460712f2393eebd9b5
Jack Andersen <jackoalan@gmail.com>
2016-07-18 07:52:17 -1000

    Add standalone bootstrap script

---
+++ standalone_bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+git clone https://github.com/AxioDL/boo.git
+pushd boo
+git submodule update --recursive --init
+popd
+
+git clone https://github.com/libAthena/athena.git
+pushd athena
+git submodule update --recursive --init
+popd
+
